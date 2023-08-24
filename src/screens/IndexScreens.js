a2e1861ccf059984880dd9f730e36b7c94caa83f
Anurag Chaudhary <anuragchaudharyM87@gmail.com>
2023-08-24 12:06:00 +0530

    added bttn
    IndexScreen able to add blog

--- src/screens/IndexScreens.js
+++ src/screens/IndexScreens.js
@@ -1,12 +1,21 @@
 import React ,{useContext}  from "react";
-import { Text,View,StyleSheet } from "react-native";
+import { Text,View,StyleSheet,FlatList, Button } from "react-native";
 import BlogContext from "../context/BlogContext";
 
 const IndexScreens=()=>{
-    const value=useContext(BlogContext)
+    const {data,addBlogPost}=useContext(BlogContext)
+
     return (
         <View>
-            <Text>Anurag {value}</Text>
+            <Text>IndexScreen</Text>
+            <Button title="AddBlog" onPress={addBlogPost} />
+            <FlatList 
+              data={data}
+              keyExtractor={(blogPost)=>blogPost.title}
+              renderItem={({item})=>{
+                return <Text>{item.title}</Text>
+              }}
+            />
         </View>
     )
 }
